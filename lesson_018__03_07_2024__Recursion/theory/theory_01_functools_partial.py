f2f58200faa85a6676a5f456e6c4062fe47f9898
ibarbylev <ibarbylev@gmail.com>
2024-07-02 19:14:03 +0300

    some fixes

--- lesson_018__03_07_2024__Recursion/theory/theory_01_functools_partial.py
+++ lesson_018__03_07_2024__Recursion/theory/theory_01_functools_partial.py
@@ -12,16 +12,20 @@
     get_total_cost_tax_rate_20(price)
 """
 from functools import partial
+from typing import Callable
 
 
 def get_total_cost(price, tax_rate):
-    return price * (1 + tax_rate)
+    new_price = price * (1 + tax_rate)
+    return round(new_price, 2)
 
 
-get_total_cost_tax_rate_0 = partial(get_total_cost, tax_rate=0)
-get_total_cost_tax_rate_9 = partial(get_total_cost, tax_rate=0.09)
-get_total_cost_tax_rate_20 = partial(get_total_cost, tax_rate=0.20)
+get_total_cost_tax_rate_0 = ...
+get_total_cost_tax_rate_9 = ...
+get_total_cost_tax_rate_20 = ...
 
 
-print(type(get_total_cost_tax_rate_20))
-print(get_total_cost_tax_rate_20(100))
+print(isinstance(get_total_cost_tax_rate_0, Callable))
+print(get_total_cost_tax_rate_0(100))   # 100
+print(get_total_cost_tax_rate_9(100))   # 109.0
+print(get_total_cost_tax_rate_20(100))  # 120.0
