fe0d811d743cc1e86a53a9a47126c04e98a40f3f
ibarbylev <ibarbylev@gmail.com>
2024-07-03 10:18:17 +0300

    some fixes

--- lesson_018__03_07_2024__recursion/total_recall/task_01.py
+++ lesson_018__03_07_2024__recursion/total_recall/task_01.py
@@ -1,10 +1,12 @@
 """Объясните, чем необходимо заменить ... , чтобы получить указанный результат? """
 
 
-def sum_items(*args): 
+def sum_items(*args: int):
+    print(type(args))
     args_str = [str(arg) for arg in args]
     print(f"{'+'.join(args_str)}={sum(args)}")
 
 
 lst = [1, 2, 3]
-sum_items(...)   # 1+2+3=6
+sum_items(*lst)   # 1+2+3=6
+sum_items(1, 2, 3)   # 1+2+3=6
