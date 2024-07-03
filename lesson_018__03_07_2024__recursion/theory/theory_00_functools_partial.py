ec99bbf2c440e767afe958cd118467363a092899
ibarbylev <ibarbylev@gmail.com>
2024-07-03 11:04:05 +0300

    some fixes

--- lesson_018__03_07_2024__recursion/theory/theory_00_functools_partial.py
+++ lesson_018__03_07_2024__recursion/theory/theory_00_functools_partial.py
@@ -3,7 +3,9 @@ from typing import Callable
 
 
 # Создание новой функции, которая всегда будет использовать аргумент x = 2
-multiply_by_two = partial(lambda x, y: x * y, 2)
+# multiply_by_two = partial(lambda x, y: x * y, 2)
+multiply_by_two = partial(lambda x, y: x * y, y=2)
+multiply_by_two = lambda x: 2 * x
 
 print(isinstance(multiply_by_two, Callable))
 result = multiply_by_two(5)
