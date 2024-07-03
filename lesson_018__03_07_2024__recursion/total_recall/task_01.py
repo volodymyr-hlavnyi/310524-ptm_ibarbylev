fca3dd04f269a738f52ced890c80f966e2aaea70
ibarbylev <ibarbylev@gmail.com>
2024-07-03 10:22:40 +0300

    some fixes

--- lesson_018__03_07_2024__recursion/total_recall/task_01.py
+++ lesson_018__03_07_2024__recursion/total_recall/task_01.py
@@ -4,9 +4,9 @@
 def sum_items(*args: int):
     print(type(args))
     args_str = [str(arg) for arg in args]
-    print(f"{'+'.join(args_str)}={sum(args)}")
+    print(f"{' + '.join(args_str)} = {sum(args)}")
 
 
 lst = [1, 2, 3]
 sum_items(*lst)   # 1+2+3=6
-sum_items(1, 2, 3)   # 1+2+3=6
+# sum_items(1, 2, 3)   # 1+2+3=6
