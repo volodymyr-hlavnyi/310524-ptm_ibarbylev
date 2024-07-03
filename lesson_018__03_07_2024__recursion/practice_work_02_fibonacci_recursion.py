6b52745a72fa632d691b3b0f163f95d5848583da
ibarbylev <ibarbylev@gmail.com>
2024-07-03 12:26:26 +0300

    some fixes

--- lesson_018__03_07_2024__recursion/practice_work_02_fibonacci_recursion.py
+++ lesson_018__03_07_2024__recursion/practice_work_02_fibonacci_recursion.py
@@ -7,11 +7,20 @@ https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%
 from functools import lru_cache
 
 
-# @lru_cache
+@lru_cache
 def fibonacci(n):
-    print(n)
-    pass
+    print(n, end=' ')
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 for i in range(7):
     print(fibonacci(i))
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
