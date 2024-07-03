b1955b92775dbb3e7eca694db5fbf71fd53a9578
ibarbylev <ibarbylev@gmail.com>
2024-07-03 10:44:52 +0300

    some fixes

--- lesson_018__03_07_2024__recursion/total_recall/task_02.py
+++ lesson_018__03_07_2024__recursion/total_recall/task_02.py
@@ -1,6 +1,7 @@
 """Дана функция numbers_squared.
 Перепишите её как лямбда-функцию numbers_squared_lambda.
 """
+from typing import Callable
 
 
 def numbers_squared(lst: list[int]) -> list[int]:
@@ -14,4 +15,6 @@ def numbers_squared(lst: list[int]) -> list[int]:
 lst = [1, 2, 3]
 print(numbers_squared(lst))   # [1, 4, 9]
 
-numbers_squared_lambda = ...
+numbers_squared_lambda: Callable = lambda lst: [item ** 2 for item in lst]
+
+print(numbers_squared_lambda(lst))
